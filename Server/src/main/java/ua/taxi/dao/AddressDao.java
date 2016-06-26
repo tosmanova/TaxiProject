9f4352b39dff2076b8864d58dbdd7f9fa72eb076
user2 <user2>
2016-06-26 11:26:30 +0300

    Added GenericDao

---
+++ Server/src/main/java/ua/taxi/dao/AddressDao.java
@@ -0,0 +1,40 @@
+package ua.artcode.taxi.dao;
+
+import ua.artcode.taxi.model.Address;
+
+import java.util.List;
+
+/**
+ * Created by serhii on 26.06.16.
+ */
+public class AddressDao implements GenericDao<Address> {
+    @Override
+    public Address create(Address el) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Address el) {
+        return false;
+    }
+
+    @Override
+    public Address findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Address> getAll(int offset, int length) {
+        return null;
+    }
+
+    @Override
+    public Address update(Address el) {
+        return null;
+    }
+
+    @Override
+    public Address getLast() {
+        return null;
+    }
+}
